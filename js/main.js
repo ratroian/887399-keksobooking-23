d19d27b4b459204c39b2c613b260bfb95da5a674
Ruslan Troyan <300493rt@gmail.com>
2021-06-04 22:43:16 +0300

    Нужно больше функий. Правки

--- js/main.js
+++ js/main.js
@@ -1,26 +1,32 @@
-// ---Случайное число из диапазона (на основе https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)---
-
-function getRandomIntInclusive(min, max) {
-  if (max > min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-  } else {
-    min = 0;
+// Случайное число из диапазона (на основе https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random)
+function getRandomInclusiveInteger(min, max) {
+  if (min === max) {
+    return Math.floor(min);
   }
-  return Math.floor(Math.random() * (max - min + 1)) + min; //Максимум и минимум включаются;
+  if (min > max) {
+    const interimInteger = min;
+    min = max;
+    max = interimInteger;
+  }
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-getRandomIntInclusive(1, 10);
+getRandomInclusiveInteger(1, 10);
 
-// ---Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно (на основе https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random и https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed)---
-
-function getRandomArbitrary(min, max, round) {
-  if (max >= min) {
-    const randomArbitrary = Math.random() * (max - min) + min;
-    return randomArbitrary.toFixed(round);
-  } else {
-    min = 0;
+// Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно (на основе https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random и https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Number/toFixed)
+function getRandomArbitraryInteger(min, max, round) {
+  if (min === max) {
+    return min.toFixed(round);
   }
+  if (min > max) {
+    const interimInteger = min;
+    min = max;
+    max = interimInteger;
+  }
+  const randomArbitrary = Math.random() * (max - min) + min;
+  return randomArbitrary.toFixed(round);
 }
 
-getRandomArbitrary(1, 2, 3);
+getRandomArbitraryInteger(1, 2, 3);

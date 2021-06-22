3766920cffd13cc09d40f3a14bd10501fb92872b
Ruslan Troyan <300493rt@gmail.com>
2021-06-23 00:23:08 +0300

    Больше деталей. Правки

--- js/utils.js
+++ js/utils.js
@@ -1,16 +1,17 @@
-function getRandomPositiveFloat (first, second, digits = 1) {
+const getRandomPositiveFloat =(first, second, digits = 1) => {
   const lower = Math.min(Math.abs(first), Math.abs(second));
   const upper = Math.max(Math.abs(first), Math.abs(second));
   const result = Math.random() * (upper - lower) + lower;
-  return result.toFixed(digits);
-}
 
-function getRandomPositiveInteger (first, second) {
+  return result.toFixed(digits);
+};
+
+const getRandomPositiveInteger =(first, second) => {
   const lower = Math.ceil(Math.min(Math.abs(first), Math.abs(second)));
   const upper = Math.floor(Math.max(Math.abs(first), Math.abs(second)));
   const result = Math.random() * (upper - lower + 1) + lower;
-  return Math.floor(result);
-}
 
-export {getRandomPositiveFloat};
-export {getRandomPositiveInteger};
+  return Math.floor(result);
+};
+
+export {getRandomPositiveFloat, getRandomPositiveInteger};

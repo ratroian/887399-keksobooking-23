079fac9667be4dd25802e7a6bec58a0e76423692
Ruslan Troyan <300493rt@gmail.com>
2021-07-06 17:34:12 +0300

    Правки

--- js/form.js
+++ js/form.js
@@ -17,8 +17,8 @@ const mapFiltersElements = mapFilters.querySelectorAll('.map__filter');
 const adFormElements = adForm.querySelectorAll('.ad-form__element');
 const mapFeatures = document.querySelector('.map__features');
 const mapFeaturesElements = mapFeatures.querySelectorAll('.map__checkbox');
-const formRoomNumber = document.querySelector('#room_number');
-const formCapacity = document.querySelector('#capacity');
+const formRoomNumberInput = document.querySelector('#room_number');
+const formCapacityInput = document.querySelector('#capacity');
 const adFormLabelInput = adForm.querySelector('#title');
 const adFormPriceInput = adForm.querySelector('#price');
 const adFormTypeList = adForm.querySelector('#type');
@@ -46,152 +46,65 @@ const activateForm = () => {
   removeAttributeDisabled(mapFeaturesElements);
 };
 
-const formCapacityArray = Array.from(formCapacity);
-const formRoomNumberArray = Array.from(formRoomNumber);
+const formCapacityArray = Array.from(formCapacityInput);
+const formRoomNumberArray = Array.from(formRoomNumberInput);
 
-// adForm.addEventListener('change', (evt) => {
 
-//   console.log(formCapacityArray[2])
+formCapacityInput.addEventListener('change', (evt) => {
+  formRoomNumberInput.value = evt.target.value;
 
-//   formRoomNumber.value = evt.target.value;
-//   formCapacity.value = evt.target.value;
-//   if (formRoomNumber[3]) {
-//     formCapacity.value = formRoomNumber[3].value
-//     formCapacityArray[2].setAttribute('disabled', 'disabled');
-//     formCapacityArray[1].setAttribute('disabled', 'disabled');
-//     formCapacityArray[0].setAttribute('disabled', 'disabled');
-//   }
-//   if (formRoomNumber.value === '2') {
-//     // formCapacity.value = evt.target.value || '2';
-//     // formCapacity.value.setAttribute('disabled', 'disabled')
-//   // } else {formCapacity.value = formCapacity.setAttribute('disabled', 'disabled');
-//   }
+  formRoomNumberArray.forEach((option) => {
+    option.setAttribute('disabled' ,'disabled');
+  });
 
-// });
-
-// adForm.addEventListener('change', (evt) => {
-//   formRoomNumber.value = evt.target.value;
-//   // formCapacity.value = evt.target.value;
-//   if (formCapacity.value === '1') {
-//     formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-//   } else {
-//     formRoomNumberArray[3].removeAttribute('disabled', 'disabled');
-//   }
-//   if (formCapacity.value === '2') {
-//     formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].setAttribute('disabled', 'disabled');
-//   } else {
-//     formRoomNumberArray[3].removeAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].removeAttribute('disabled', 'disabled');
-//   }
-//   if (formCapacity.value === '3') {
-//     formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[1].setAttribute('disabled', 'disabled');
-//   } else {
-//     formRoomNumberArray[3].removeAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].removeAttribute('disabled', 'disabled');
-//     formRoomNumberArray[1].removeAttribute('disabled', 'disabled');
-//   }
-// });
-
-// formCapacity.addEventListener('change', (evt) => {
-//   // formRoomNumber.value = evt.target.value;
-//   if (formCapacity.value === '1') {
-//     formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-//   } else {
-//     formRoomNumberArray[3].removeAttribute('disabled', 'disabled');
-//   }
-//   if (formCapacity.value === '2') {
-//     formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].setAttribute('disabled', 'disabled');
-//   } else {
-//     // formRoomNumberArray[3].removeAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].removeAttribute('disabled', 'disabled');
-//   }
-//   if (formCapacity.value === '3') {
-//     formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[1].setAttribute('disabled', 'disabled');
-//   } else {
-//     // formRoomNumberArray[3].removeAttribute('disabled', 'disabled');
-//     // formRoomNumberArray[0].removeAttribute('disabled', 'disabled');
-//     formRoomNumberArray[1].removeAttribute('disabled', 'disabled');
-//   }
-//   console.log(1)
-
-// });
-
-// formCapacity.addEventListener('change', (evt) => {
-//   // formRoomNumber.value = evt.target.value;
-//   if (formCapacity.value === '1') {
-//     formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-//   } else if (formCapacity.value === '2') {
-//     formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].setAttribute('disabled', 'disabled');
-//   } else if (formCapacity.value === '3') {
-//     formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[1].setAttribute('disabled', 'disabled');
-//   } else if (formCapacity.value === '0') {
-//     formRoomNumberArray[2].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].setAttribute('disabled', 'disabled');
-//     formRoomNumberArray[1].setAttribute('disabled', 'disabled');
-//   } else {
-//     formRoomNumberArray[2].removeAttribute('disabled', 'disabled');
-//     formRoomNumberArray[0].removeAttribute('disabled', 'disabled');
-//     formRoomNumberArray[1].removeAttribute('disabled', 'disabled');
-//     formRoomNumberArray[3].removeAttribute('disabled', 'disabled');
-//   }
-
-//   console.log(1)
-
-// });
-
-formCapacity.addEventListener('change', (evt) => {
-  formRoomNumber.value = evt.target.value;
-  formRoomNumberArray[0].setAttribute('disabled', 'disabled');
-  formRoomNumberArray[1].setAttribute('disabled', 'disabled');
-  formRoomNumberArray[2].setAttribute('disabled', 'disabled');
-  formRoomNumberArray[3].setAttribute('disabled', 'disabled');
-  if (formCapacity.value === '1') {
-    formRoomNumberArray[0].removeAttribute('disabled', 'disabled');
-    formRoomNumberArray[1].removeAttribute('disabled', 'disabled');
-    formRoomNumberArray[2].removeAttribute('disabled', 'disabled');
-    formCapacity.setCustomValidity('Вы можете выбрать номера на 1, 2, 3 комнаты');
-  } else if (formCapacity.value === '2') {
-    formRoomNumberArray[1].removeAttribute('disabled', 'disabled');
-    formRoomNumberArray[2].removeAttribute('disabled', 'disabled');
-    formCapacity.setCustomValidity('Вы можете выбрать номера на 2, 3 комнаты');
-  } else if (formCapacity.value === '3') {
-    formRoomNumberArray[2].removeAttribute('disabled', 'disabled');
-    formCapacity.setCustomValidity('Вы можете выбрать номера на 3 комнаты');
-  } else if (formCapacity.value === '0') {
-    formRoomNumber.value = '100';
-    formRoomNumberArray[3].removeAttribute('disabled', 'disabled');
-  } else {
-    formCapacity.setCustomValidity('');
+  switch (formCapacityInput.value) {
+    case '1':
+      formRoomNumberArray[0].removeAttribute('disabled', 'disabled');
+      formRoomNumberArray[1].removeAttribute('disabled', 'disabled');
+      formRoomNumberArray[2].removeAttribute('disabled', 'disabled');
+      formCapacityInput.setCustomValidity('Вы можете выбрать номера на 1, 2, 3 комнаты');
+      break;
+    case '2':
+      formRoomNumberArray[1].removeAttribute('disabled', 'disabled');
+      formRoomNumberArray[2].removeAttribute('disabled', 'disabled');
+      formCapacityInput.setCustomValidity('Вы можете выбрать номера на 2, 3 комнаты');
+      break;
+    case '3':
+      formRoomNumberArray[2].removeAttribute('disabled', 'disabled');
+      formCapacityInput.setCustomValidity('Вы можете выбрать номера на 3 комнаты');
+      break;
+    case '0':
+      formRoomNumberInput.value = '100';
+      formRoomNumberArray[3].removeAttribute('disabled', 'disabled');
+      break;
+    default:
+      formCapacityInput.setCustomValidity('');
   }
 });
 
-formRoomNumber.addEventListener('change', (evt) => {
-  formCapacity.value = evt.target.value;
-  formCapacityArray[0].setAttribute('disabled', 'disabled');
-  formCapacityArray[1].setAttribute('disabled', 'disabled');
-  formCapacityArray[2].setAttribute('disabled', 'disabled');
-  formCapacityArray[3].setAttribute('disabled', 'disabled');
-  if (formRoomNumber.value === '1') {
-    formRoomNumberArray[0].removeAttribute('disabled', 'disabled');
-  } else if (formRoomNumber.value === '2') {
-    formCapacityArray[0].removeAttribute('disabled', 'disabled');
-    formCapacityArray[1].removeAttribute('disabled', 'disabled');
-  } else if (formRoomNumber.value === '3') {
-    formCapacityArray[0].removeAttribute('disabled', 'disabled');
-    formCapacityArray[1].removeAttribute('disabled', 'disabled');
-    formCapacityArray[2].removeAttribute('disabled', 'disabled');
-  } else if (formRoomNumber.value === '100') {
-    formCapacity.value = '0';
-    formCapacityArray[3].removeAttribute('disabled', 'disabled');
+formRoomNumberInput.addEventListener('change', (evt) => {
+  formCapacityInput.value = evt.target.value;
+
+  formCapacityArray.forEach((option) => {
+    option.setAttribute('disabled' ,'disabled');
+  });
+
+  switch (formRoomNumberInput.value) {
+    case '1':
+      formRoomNumberArray[0].removeAttribute('disabled', 'disabled');
+      break;
+    case '2':
+      formCapacityArray[0].removeAttribute('disabled', 'disabled');
+      formCapacityArray[1].removeAttribute('disabled', 'disabled');
+      break;
+    case '3':
+      formCapacityArray[0].removeAttribute('disabled', 'disabled');
+      formCapacityArray[1].removeAttribute('disabled', 'disabled');
+      formCapacityArray[2].removeAttribute('disabled', 'disabled');
+      break;
+    case '100':
+      formCapacityInput.value = '0';
+      formCapacityArray[3].removeAttribute('disabled', 'disabled');
   }
 });
 
@@ -199,9 +112,9 @@ adFormLabelInput.addEventListener('input', () => {
   const valueLength = adFormLabelInput.value.length;
 
   if (valueLength < MIN_LABEL_LENGTH) {
-    adFormLabelInput.setCustomValidity(`Ещё ${  MIN_LABEL_LENGTH - valueLength } симв.`);
+    adFormLabelInput.setCustomValidity(`Ещё ${MIN_LABEL_LENGTH - valueLength} симв.`);
   } else if (valueLength > MAX_LABEL_LENGTH) {
-    adFormLabelInput.setCustomValidity(`Удалите лишние ${  valueLength - MAX_LABEL_LENGTH } симв.`);
+    adFormLabelInput.setCustomValidity(`Удалите лишние ${valueLength - MAX_LABEL_LENGTH} симв.`);
   } else {
     adFormLabelInput.setCustomValidity('');
   }
@@ -219,12 +132,16 @@ adFormPriceInput.addEventListener('input', () => {
   adFormPriceInput.reportValidity();
 });
 
-adFormTypeList.addEventListener('change', function () {
-  const minPriceValue = OFFER_TYPES_PRICE[this.value];
-  adFormPriceInput.value = OFFER_TYPES_PRICE[this.value];
+adFormTypeList.addEventListener('change', (evt) => {
+  const minPriceValue = OFFER_TYPES_PRICE[evt.target.value];
+  adFormPriceInput.value = OFFER_TYPES_PRICE[evt.target.value];
+
   adFormPriceInput.setAttribute('min', minPriceValue);
+
   adFormPriceInput.value = Math.max(minPriceValue, adFormPriceInput.value);
-  adFormTypeList.setCustomValidity(`Минимальная цена за ночь ${  OFFER_TYPES_PRICE[this.value] } руб.`);
+
+  adFormTypeList.setCustomValidity(`Минимальная цена за ночь ${OFFER_TYPES_PRICE[evt.target.value]} руб.`);
+
   adFormTypeList.reportValidity();
 });
 
